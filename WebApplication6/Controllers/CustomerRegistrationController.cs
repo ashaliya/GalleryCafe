c25dfafc50073bbb3990627b3b87b31fdd8bfd6b
ashal <ashal@ASHALIYA>
2024-08-10 19:52:32 +0530

    Add project files.

---
+++ WebApplication6/Controllers/CustomerRegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class CustomerRegistrationController : Controller
+    {
+        // GET: CustomerRegistration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
